2189470c453a8920ffac0f236cfbf804f9fbffde
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-08-05 22:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-function getFormvalue(event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
+const form = document.getElementById("form1");
+form.addEventListener("submit",getFormvalue);
+function getFormvalue() {
+    //Write your code here
 
-    // Get the values from the form inputs
-    const firstName = document.querySelector('input[name="fname"]').value;
-    const lastName = document.querySelector('input[name="lname"]').value;
-
-    // Create the output string with first and last names
-    const output = `${firstName} ${lastName}`;
-
-    // Show an alert with the formatted output
-    alert(output);
+	alert(form.fname.value+" "+form.lname.value);
 }
